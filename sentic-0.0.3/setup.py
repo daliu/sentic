5ec7ec46dbe3b3776f71dc48325c4dbdd9956abb
Dave Liu <7david12liu@gmail.com>
2018-01-28 23:01:01 -0800

    ./update_script.sh

---
+++ sentic-0.0.3/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = ['sentic', 'sentic.babel']
+
+setup(name='sentic',
+      version='0.0.3',
+      description='Sentic Package for NLP',
+      long_description=open('README.md').read(),
+      author=u'David Liu',
+      author_email='7david12liu@gmail.com',
+      url='https://github.com/daliu/sentic/',
+      packages=packages,
+      package_data={'': ['LICENSE', 'README.md'], 'sentic': ['sentic'], 'sentic.babel': ['babel']},
+      package_dir={'sentic': 'sentic', 'sentic.babel': 'sentic/babel'},
+      include_package_data=True,
+      license='MIT',
+      zip_safe=False,
+      install_requires=[],
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries'],
+)
